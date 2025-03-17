e7a29d188540893ce586f1d536b89bef97a8cb94
Rodrigo <rodrigo_ladaga@hotmail.com>
2025-03-17 13:35:54 -0300

    rename secrets to avoid conflict

--- bot/config.py
+++ bot/config.py
@@ -1,8 +1,9 @@
 import importlib.util
 
+
 def load_config():
-    """Load configuration from """
-    spec = importlib.util.spec_from_file_location('secrets', 'secrets.py')
+    spec = importlib.util.spec_from_file_location(
+        'secrets', 'config_secrets.py')
     secrets_module = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(secrets_module)
 
