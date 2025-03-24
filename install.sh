9d9c5cb11c250ef38cebca2de77e72641242c296
juanrunzio <jbauti9@gmail.com>
2025-03-24 19:28:18 -0300

    update the install script now systemd handles service path

--- install.sh
+++ install.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-# Install necessary package
+# Install necessary packages
 sudo pacman -S python python-pip python-virtualenv
 
 # Create a virtual environment
 python -m venv venv
 
-# Install the required packages
+# Install required packages
 venv/bin/pip install -r requirements.txt
 
 # Create systemd service file
-
-SERVICE_PATH="/etc/systemd/system/mercourier.service"
 CURRENT_DIR=$(pwd)
+SERVICE_FILE="$CURRENT_DIR/mercourier.service"
 
 SERVICE_CONTENT="[Unit]
 Description=GitHub to Zulip Notification Bot
@@ -20,25 +19,20 @@ After=network.target
 
 [Service]
 Type=simple
-User=root
 WorkingDirectory=${CURRENT_DIR}
 Environment=PATH=${CURRENT_DIR}/venv/bin
 ExecStart=${CURRENT_DIR}/venv/bin/python ${CURRENT_DIR}/main.py
 Restart=always
 
 [Install]
-WantedBy=multi-user.target"
+WantedBy=default.target"
 
-echo "$SERVICE_CONTENT" | sudo tee "$SERVICE_PATH" > /dev/null
+# Write the service file
+echo "$SERVICE_CONTENT" > "$SERVICE_FILE"
 
-# Reload systemd
-sudo systemctl daemon-reload
+# Enable, Reload and Start the mercourier.service
+systemctl --user enable "$SERVICE_FILE"
+systemctl --user daemon-reload
+systemctl --user start mercourier
 
-# Enable the service
-sudo systemctl enable mercourier
-
-# Start the service
-sudo systemctl start mercourier
-
-# Display the status of the service
-sudo systemctl status mercourier
+systemctl --user status mercourier
