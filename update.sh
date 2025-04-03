debb660a295c6df8424d10ada1671c6c3d2291b6
Rodrigo <rodrigo_ladaga@hotmail.com>
2025-04-03 16:43:35 -0300

    remove parameter

--- update.sh
+++ update.sh
@@ -4,14 +4,12 @@ set -ex
 
 BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
 SERVICE_NAME="mercourier-${BRANCH_NAME}.service"
-# Stop the service
 
+# Stop the service
 systemctl --user stop ${SERVICE_NAME}
 
 # Pull the latest changes from the repository
-
 git pull origin ${BRANCH_NAME}
 
 # Start the service
-
 systemctl --user start ${SERVICE_NAME}
