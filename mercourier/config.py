ae228b70b8c80d133e7e7eead63bc1b867161dd6
rladaga <93140602+rladaga@users.noreply.github.com>
2025-04-28 15:42:35 -0300

    rename

--- mercourier/config.py
+++ mercourier/config.py
@@ -1,7 +1,7 @@
 import importlib.util
 
 
-def load_config(path="config_secrets.py"):
+def load_config(path="config_and_secrets.py"):
     spec = importlib.util.spec_from_file_location("secrets", path)
     secrets_module = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(secrets_module)
