51a7c75b7ca5bbe380bbfbee5540276d2bba3e92
rladaga <93140602+rladaga@users.noreply.github.com>
2025-04-28 16:11:03 -0300

    test remove html comments on body

--- mercourier/__init__.py
+++ mercourier/__init__.py
@@ -4,4 +4,5 @@ from .config import load_config as load_config
 from .template import (
     rewrite_issue_numbers as rewrite_issue_numbers,
     rewrite_github_issue_urls as rewrite_github_issue_urls,
+    remove_html_comments as remove_html_comments,
 )
